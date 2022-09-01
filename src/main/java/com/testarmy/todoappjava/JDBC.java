a9db7ccfc2b17dda4ad363da37fc2676f7fe0a85
NiceLeader <maciek2002.10@wp.pl>
2022-09-01 15:37:43 +0200

    Added things

--- src/main/java/com/testarmy/todoappjava/JDBC.java
+++ src/main/java/com/testarmy/todoappjava/JDBC.java
@@ -2,17 +2,48 @@ package com.testarmy.todoappjava;
 import java.sql.*;
 
 public class JDBC {
-    public static void main(String[] args) {
-        try {
-            Connection connection =
-                    DriverManager.getConnection(
-                            "jdbc:mysql://localhost:3306/to_do_app",
-                            "root",
-                            ""
-                    );
+private static  String DATABASE_URL = "jdbc:mysql://localhost:3306/to_do_app";
+    private static  String DATABASE_USERNAME = "root";
+    private static  String DATABASE_PASSWORD = "";
+    private static  String SELECT_QUERY = "SELECT * FROM registration WHERE email_id = ? and password = ?";
+
+
+    public boolean validate(String emailId, String password) throws SQLException {
+
+        try (Connection connection = DriverManager
+                .getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY)) {
+            preparedStatement.setString(1, emailId);
+            preparedStatement.setString(2, password);
+
+            System.out.println(preparedStatement);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            }
+
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            printSQLException(e);
         }
+        return false;
     }
 
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/testarmy/todoappjava/ToDoController.java
+++ src/main/java/com/testarmy/todoappjava/ToDoController.java
@@ -2,10 +2,10 @@ package com.testarmy.todoappjava;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.Window;
+
+import java.sql.SQLException;
 
 public class ToDoController {
     @FXML
@@ -15,6 +15,52 @@ public class ToDoController {
     @FXML
     private Button submitButton;
 
-    public void login(ActionEvent actionEvent) {
+    @FXML
+    public void login(ActionEvent event) throws SQLException {
+
+        Window owner = submitButton.getScene().getWindow();
+
+        System.out.println(emailIdField.getText());
+        System.out.println(passwordField.getText());
+
+        if (emailIdField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter your email id");
+            return;
+        }
+        if (passwordField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter a password");
+            return;
+        }
+
+        String emailId = emailIdField.getText();
+        String password = passwordField.getText();
+
+        JDBC jdbc = new JDBC();
+        boolean flag = jdbc.validate(emailId, password);
+
+        if (!flag) {
+            infoBox("Type correct e-mail and password", null, "Failed");
+        } else {
+            infoBox("Login Successful!", null, "Failed");
+        }
+    }
+
+    public static void infoBox(String infoMessage, String headerText, String title) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    }
+
+    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
     }
 }
