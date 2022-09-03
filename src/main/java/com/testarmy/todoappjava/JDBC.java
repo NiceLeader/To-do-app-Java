26b0cd18cf4aae2d483a123de93caad340f0bf7c
marek wasiewski <108329227+marwas97@users.noreply.github.com>
2022-09-03 15:03:38 +0200

    creating tables kinda works

--- src/main/java/com/testarmy/todoappjava/JDBC.java
+++ src/main/java/com/testarmy/todoappjava/JDBC.java
@@ -47,58 +47,59 @@ private static  String DATABASE_URL = "jdbc:mysql://localhost:3306/to_do_app";
             }
         }
     }
-    private static void createTask() throws SQLException {
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:8012/to_do_app",
-                "root",
-                "")){
-            Statement statement = connection.createStatement();
-        String sqlCreateTask = """
-            CREATE TABLE task(
-                    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                    title VARCHAR(255) NOT NULL,
-                    description TEXT NOT NULL,
-                    done BOOLEAN NOT NULL DEFAULT FALSE,
-                    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                    priority ENUM ('high', 'normal', 'low') NOT NULL DEFAULT 'low',
-                    user_id INT NOT NULL,
-                    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE);
-                                         """;
-        statement.executeUpdate(sqlCreateTask);
-    }
-    public static void addTask(Connection connection) throws SQLException{
-            try (Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/to_do_list",
-                    "root",
-                    "Grupa03!")){
-                Statement statement = connection.createStatement();
-
-        ResultSet resultSetInsert = statement.getGeneratedKeys();
-        String sqlUpdate = """
-          INSERT INTO task(title,description,priority,user_id) VALUES
-         (?,?,?,?)
-             """;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("podaj nazwę zadania");
-        String title = scanner.nextLine();
-        System.out.println("podaj opis zadania");
-        String description = scanner.nextLine();
-        System.out.println("podaj priorytet zadania");
-        String priority = scanner.nextLine();
-        System.out.println("podaj numer użytkownika");
-        String userId = scanner.nextLine();
-        PreparedStatement preparedStatement = connection.prepareStatement(sqlUpdate);
-        preparedStatement.setString(1,title);
-        preparedStatement.setString(2,description);
-        preparedStatement.setString(3,priority);
-        preparedStatement.setInt(4,Integer.parseInt(userId));
-
-        preparedStatement.executeUpdate();
-
-        if(resultSetInsert != null && resultSetInsert.next()) {
-            System.out.println("test: "+resultSetInsert.getInt(1));
+    private static void createTask(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+            String sqlCreateTask = """
+                    CREATE TABLE task(
+                            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            title VARCHAR(255) NOT NULL,
+                            description TEXT NOT NULL,
+                            done BOOLEAN NOT NULL DEFAULT FALSE,
+                            created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                            priority ENUM ('high', 'normal', 'low') NOT NULL DEFAULT 'low',
+                            user_id INT NOT NULL,
+                            FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE);
+                                                 """;
+            statement.executeUpdate(sqlCreateTask);
         }
-    }
+
+        private static void createTableUsers(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+            String sqlCreateTask = """
+                    CREATE TABLE IF NOT EXIST user(
+                            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            name VARCHAR(128) NOT NULL
+                            surname VARCHAR(255) NOT NULL
+                            login VARCHAR(128) NOT NULL UNIQUE
+                            password CHAR(60) NOT NULL
+                            email VARCHAR(255) UNIQUE);
+                                                 """;
+            statement.executeUpdate(sqlCreateTask);
+        }
+
+    public static void addTask(Connection connection) throws SQLException {
+            Statement statement = connection.createStatement();
+            ResultSet resultSetInsert = statement.getGeneratedKeys();
+            String sqlUpdate = """
+                     INSERT INTO task(title,description,priority,user_id) VALUES
+                    (?,?,?,?)
+                        """;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("podaj nazwę zadania");
+            String title = scanner.nextLine();
+            System.out.println("podaj opis zadania");
+            String description = scanner.nextLine();
+            System.out.println("podaj priorytet zadania");
+            String priority = scanner.nextLine();
+            System.out.println("podaj numer użytkownika");
+            String userId = scanner.nextLine();
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlUpdate);
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, description);
+            preparedStatement.setString(3, priority);
+            preparedStatement.setInt(4, Integer.parseInt(userId));
+            preparedStatement.executeUpdate();
+        }
     private static void update(Connection connection) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("podaj id zadania");
@@ -111,4 +112,11 @@ private static  String DATABASE_URL = "jdbc:mysql://localhost:3306/to_do_app";
         preparedStatement.setInt(2,Integer.parseInt(id));
         preparedStatement.executeUpdate();
     }
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY);
+        createTask(connection);
+    }
 }
+
