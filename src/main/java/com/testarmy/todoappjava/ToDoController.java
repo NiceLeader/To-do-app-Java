b5264389071f682a8d94f2a46100677bf9108656
marek wasiewski <108329227+marwas97@users.noreply.github.com>
2022-09-04 11:42:48 +0200

    2.8

--- src/main/java/com/testarmy/todoappjava/ToDoController.java
+++ src/main/java/com/testarmy/todoappjava/ToDoController.java
@@ -3,30 +3,67 @@ package com.testarmy.todoappjava;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 import javafx.stage.Window;
+import org.mindrot.jbcrypt.BCrypt;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import static com.testarmy.todoappjava.JDBC.*;
 
 public class ToDoController {
+
+
+
     @FXML
-    private TextField emailIdField;
+    private TextField loginField;
     @FXML
     private PasswordField passwordField;
     @FXML
     private Button submitButton;
     @FXML
-    private Button test;
+    private Button register;
+
+    public ToDoController() throws SQLException {
+    }
 
     @FXML
-    public void testButton(ActionEvent event) throws SQLException {
+    public void registerButton(ActionEvent event, Stage stage) throws SQLException, IOException {
         Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
         connection.setCatalog("to_do_app");
         System.out.println("obecna baza danych: "+connection.getCatalog());
-        showAllColumnsFromResultSet(searchTask(connection));
+    }
+    @FXML
+    public void loginUser(ActionEvent event) throws SQLException {
+        Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+        connection.setCatalog("to_do_app");
+        Scanner scannerLogin = new Scanner(loginField.getText());
+        System.out.println("podaj login");
+        String login = scannerLogin.nextLine();
+        scannerLogin.close();
+        Scanner scannerPassword = new Scanner(passwordField.getText());
+        System.out.println("podaj hasło");
+        String password = scannerPassword.nextLine();
+        scannerPassword.close();
+        String sqlUser = "SELECT id,name,password FROM user WHERE login=? AND password=?;";
+        PreparedStatement statement = connection.prepareStatement(sqlUser);
+        String sqlPswd = "SELECT password FROM user WHERE login=?;";
+        PreparedStatement pswd = connection.prepareStatement(sqlPswd);
+        pswd.setString(1, login);
+        pswd.executeUpdate();
+        statement.setString(1, login);
+        if (BCrypt.checkpw(password, sqlPswd)) {
+            statement.setString(2, password);
+//            String sqlName = "SELECT name FROM user WHERE login=?;";
+//            PreparedStatement name = connection.prepareStatement(sqlName);
+//            name.setString(1,login);
+            statement.executeUpdate();
+        } System.out.println("błędne hasło");
     }
 
     @FXML
@@ -34,10 +71,10 @@ public class ToDoController {
 
         Window owner = submitButton.getScene().getWindow();
 
-        System.out.println(emailIdField.getText());
+        System.out.println(loginField.getText());
         System.out.println(passwordField.getText());
 
-        if (emailIdField.getText().isEmpty()) {
+        if (loginField.getText().isEmpty()) {
             showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
                     "Please enter your email id");
             return;
@@ -48,7 +85,7 @@ public class ToDoController {
             return;
         }
 
-        String emailId = emailIdField.getText();
+        String emailId = loginField.getText();
         String password = passwordField.getText();
 
         JDBC jdbc = new JDBC();
@@ -77,4 +114,6 @@ public class ToDoController {
         alert.initOwner(owner);
         alert.show();
     }
-}
+
+
+    }
